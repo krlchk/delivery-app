8f94b1d958841f283537ee3d03174ce69cc12d01
Kirill Revunov <krrevunov@gmail.com>
2025-09-18 16:33:08 +0300

    backend: implemented project architecture

---
+++ backend/src/config/db.ts
@@ -0,0 +1,20 @@
+import pgk from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+pool.on("connect", () => {
+  console.log("Connection pool established with database");
+});
+
+export default pool;
---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`);
+});
